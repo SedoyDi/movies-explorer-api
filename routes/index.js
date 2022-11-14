ce2b476dfd0bf38331533985aa8c62fbc70cd8ae
SedoyDi <95245699+SedoyDi@users.noreply.github.com>
2022-11-14 23:00:39 +0300

    fix: app.js import validation

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const auth = require('../middlewares/auth');
 const userRouter = require('./users');
 const movieRouter = require('./movies');
-const { authValidation, regValidation } = require('../middlewares/validation');
+const { validatAuth, validatReg } = require('../middlewares/validation');
 const { login, createUser } = require('../controllers/users');
 const AuthError = require('../errors/authError');
 const { neededAutorisation } = require('../utils/eroorMessage');
 
-router.post('/signin', authValidation, login);
-router.post('/signup', regValidation, createUser);
+router.post('/signin', validatAuth, login);
+router.post('/signup', validatReg, createUser);
 
 router.use(auth);
 router.use('/users', userRouter);
